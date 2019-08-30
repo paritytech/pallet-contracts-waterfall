5a2550576d068556404369e323e38c5205f05b84
Sergey Pepyakin <sergei@parity.io>
2019-08-30 17:27:38 +0200

    Add the simplest contract

---
+++ contracts/raw-incrementer/src/ext.rs
@@ -0,0 +1,71 @@
+
+use alloc::vec::Vec;
+
+mod cabi {
+    extern "C" {
+        pub fn ext_set_storage(key_ptr: u32, value_non_null: u32, value_ptr: u32, value_len: u32);
+        pub fn ext_get_storage(key_ptr: u32) -> u32;
+        pub fn ext_scratch_size() -> u32;
+        pub fn ext_scratch_read(dest_ptr: u32, offset: u32, len: u32);
+        pub fn ext_scratch_write(src_ptr: u32, len: u32);
+    }
+}
+
+pub struct Key(pub [u8; 32]);
+
+pub fn set_storage(key: &Key, value: Option<&[u8]>) {
+    unsafe {
+        let mut value_ptr = 0;
+        let mut value_len = 0;
+        let value_non_null = if let Some(v) = value {
+            value_ptr = v.as_ptr() as u32;
+            value_len = v.len() as u32;
+            1
+        } else {
+            0
+        };
+
+        cabi::ext_set_storage(key.0.as_ptr() as u32, value_non_null, value_ptr, value_len);
+    }
+}
+
+
+pub fn get_storage(key: &Key) -> Option<Vec<u8>> {
+    const ERR_OK: u32 = 0;
+    unsafe {
+        let result = cabi::ext_get_storage(key.0.as_ptr() as u32);
+        if result == ERR_OK {
+            let size = cabi::ext_scratch_size();
+            let mut value = Vec::new();
+            if size > 0 {
+                value.resize(size as usize, 0);
+                cabi::ext_scratch_read(value.as_mut_ptr() as u32, 0, size);
+            }
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
+
+pub fn scratch_buf() -> Vec<u8> {
+    unsafe {
+        let size = cabi::ext_scratch_size();
+        if size == 0 {
+            return Vec::new();
+        }
+
+        let mut value = Vec::new();
+        if size > 0 {
+            value.resize(size as usize, 0);
+            cabi::ext_scratch_read(value.as_mut_ptr() as u32, 0, size);
+        }
+        value
+    }
+}
+
+pub fn scratch_buf_set(data: &[u8]) {
+    unsafe {
+        cabi::ext_scratch_write(data.as_ptr() as u32, data.len() as u32);
+    }
+}
---
+++ contracts/raw-incrementer/src/lib.rs
@@ -0,0 +1,84 @@
+//! Implementation of a simple smart-contract using only raw APIs.
+
+#![feature(alloc_error_handler)]
+
+#![no_std]
+
+#[macro_use]
+extern crate parity_codec_derive;
+extern crate parity_codec as codec;
+extern crate wee_alloc;
+
+#[macro_use]
+extern crate alloc;
+
+use core::arch::wasm32;
+use alloc::vec::Vec;
+use codec::{Encode, Decode};
+
+mod ext;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[panic_handler]
+#[no_mangle]
+pub fn panic(_info: &::core::panic::PanicInfo) -> ! {
+	unsafe {
+		wasm32::unreachable()
+	}
+}
+
+#[alloc_error_handler]
+pub extern fn oom(_: ::core::alloc::Layout) -> ! {
+	unsafe {
+		wasm32::unreachable()
+	}
+}
+
+#[derive(Encode, Decode)]
+enum Action {
+    Inc(u32),
+    Get,
+}
+
+static COUNTER_KEY: ext::Key = ext::Key([1; 32]);
+
+fn handle(input: &[u8]) -> Vec<u8> {
+    let action = Action::decode(&mut &input[..]).unwrap();
+
+    match action {
+        Action::Inc(by) => {
+            let mut counter = ext::get_storage(&COUNTER_KEY).and_then(|v| u32::decode(&mut &v[..])).unwrap_or(0);
+            counter += by;
+            ext::set_storage(&COUNTER_KEY, Some(&u32::encode(&counter)));
+            Vec::new()
+        }
+        Action::Get => {
+            let raw_counter = ext::get_storage(&COUNTER_KEY).unwrap_or(vec![]);
+            raw_counter.to_vec()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn call() -> u32 {
+    // Get the input data for the execution. It is placed into the scratch buffer.
+    let input = ext::scratch_buf();
+
+    // Handle the message.
+    let output = handle(&input);
+
+    // Always overwrite the scratch buffer.
+    ext::scratch_buf_set(&output);
+
+    // Return 0 (ERR_OK)
+    0
+}
+
+// We need to define `deploy` function, so the wasm-build will produce a constructor binary.
+#[no_mangle]
+pub extern "C" fn deploy() -> u32 {
+    0
+}
