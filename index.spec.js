a1426cec00be82530eb5c2c51aea5dfc14733017
Sergey Pepyakin <sergei@parity.io>
2019-08-30 14:09:58 +0200

    Fully verify

--- index.spec.js
+++ index.spec.js
@@ -1,6 +1,5 @@
 const { ApiPromise, WsProvider, Bytes } = require('@polkadot/api');
 const { Abi } = require('@polkadot/api-contract');
-const Rpc = require('@polkadot/rpc-core').default;
 const testKeyring = require('@polkadot/keyring/testing');
 const { randomAsU8a } = require('@polkadot/util-crypto');
 const BN = require('bn.js');
@@ -9,24 +8,22 @@ const fs = require('fs');
 const ALICE = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
 const DOT = new BN('1000000000000000');
 
-async function getContractStorage(api, rpc, contractAddress, storageKey) {
+async function getContractStorage(api, contractAddress, storageKey) {
     let contractInfo = await api.query.contracts.contractInfoOf(contractAddress);
-    let trieId = contractInfo.unwarp().asAlive.trieId;
-    return await rpc.state.getChildStorage(trieId, storageKey).toPromise();
+    let trieId = contractInfo.unwrap().asAlive.trieId.toString('hex');
+    return await api.rpc.state.getChildStorage(trieId, storageKey);
 }
 
 describe('simplest contract', () => {
     // This is a test account that is going to be created and funded each test.
     let testOrigin;
     let api;
-    let rpc;
 
     beforeEach(async (done) => {
         jest.setTimeout(25000);
 
         const provider = new WsProvider('ws://127.0.0.1:9944');
         api = await ApiPromise.create({ provider });
-        rpc = new Rpc(provider);
 
         const keyring = testKeyring.default();
         const alicePair = keyring.getPair(ALICE);
@@ -83,31 +80,24 @@ describe('simplest contract', () => {
 
                             // Flip flag
                             let flipFlag = await getContractStorage(
-                                 api,
-                                 rpc,
-                                 contractAddress,
-                                 '0xeb72c87e65bed3596d6fef83aeb784615cdac1be1328adf1c7336acd6ba9ff77'
+                                api,
+                                contractAddress,
+                                '0xeb72c87e65bed3596d6fef83aeb784615cdac1be1328adf1c7336acd6ba9ff77'
                             );
                             console.log(flipFlag); // should be 0
 
-                            done(); // TODO: Remove
-
-                            // 5G5YthgPLGcHkjp9Q5SFLuV6kD7T3S25W1wSxEWaRaVUy1Vj
-
-                            // api.tx.contracts.call(contractAddress, 0, 500000, abi.messages.flip())
-                            //     .signAndSend(testOrigin, (result) => {
-                            //         if (result.status.isFinalized) {
-                            //             // need to call get child storage with
-                            //             // 1. trie_id
-                            //             // 2. the key
-                            //             //
-                            //             // the key can be hardcoded,
-                            //             // the child storage id can be obtained by a contract address.
-
-                            //             // TODO: Verify that the state is actually flipped.
-                            //             done();
-                            //         }
-                            //     });
+                            api.tx.contracts.call(contractAddress, 0, 500000, abi.messages.flip())
+                                .signAndSend(testOrigin, async (result) => {
+                                    if (result.status.isFinalized) {
+                                        let flipFlag = await getContractStorage(
+                                            api,
+                                            contractAddress,
+                                            '0xeb72c87e65bed3596d6fef83aeb784615cdac1be1328adf1c7336acd6ba9ff77'
+                                        );
+                                        console.log(flipFlag);
+                                        done();
+                                    }
+                                });
                         }
                     }
                 });
