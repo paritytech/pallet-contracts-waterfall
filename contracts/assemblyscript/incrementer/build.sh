d8e9f384f51b016766606f5bd7379c4782f313fb
Stefanie Doll <stefie.doll@googlemail.com>
2019-10-02 16:50:45 +0200

    Interim solution to overwrite AssemblyScript abort function

--- contracts/assemblyscript/incrementer/build.sh
+++ contracts/assemblyscript/incrementer/build.sh
@@ -5,5 +5,11 @@ set -e
 PROJNAME=incrementer
 
 cat build/$PROJNAME.wat | sed "s/(import \"env\" \"memory\" (memory \$0 1))/(import \"env\" \"memory\" (memory \$0 2 16))/" > build/$PROJNAME-fixed.wat
-wat2wasm -o build/$PROJNAME-fixed.wasm build/$PROJNAME-fixed.wat
-wasm-prune --exports call,deploy build/$PROJNAME-fixed.wasm build/$PROJNAME-pruned.wasm
+
+cat build/$PROJNAME-fixed.wat | sed "/(import \"env\" \"abort\" (func \$~lib\/builtins\/abort (param i32 i32 i32 i32)))/d" > build/$PROJNAME-trap.wat
+
+cat build/$PROJNAME-trap.wat | sed "/call $~lib\/builtins\/abort/d" > build/$PROJNAME-trap2.wat
+
+wat2wasm -o build/$PROJNAME-trap2.wasm build/$PROJNAME-trap2.wat
+
+wasm-prune --exports call,deploy build/$PROJNAME-trap2.wasm build/$PROJNAME-pruned.wasm
