aa4ef31bc3eb1a694fac5b0649868bb8d080d888
Stefanie Doll <stefie.doll@googlemail.com>
2019-10-02 15:25:31 +0200

    Overwrite minimum memory pages and set maximum

--- contracts/assemblyscript/incrementer/build.sh
+++ contracts/assemblyscript/incrementer/build.sh
@@ -4,6 +4,6 @@ set -e
 
 PROJNAME=incrementer
 
-cat build/$PROJNAME.wat | sed "s/(import \"env\" \"memory\" (memory (;0;) 2))/(import \"env\" \"memory\" (memory (;0;) 2 16))/" > build/$PROJNAME-fixed.wat
+cat build/$PROJNAME.wat | sed "s/(import \"env\" \"memory\" (memory \$0 1))/(import \"env\" \"memory\" (memory \$0 2 16))/" > build/$PROJNAME-fixed.wat
 wat2wasm -o build/$PROJNAME-fixed.wasm build/$PROJNAME-fixed.wat
 wasm-prune --exports call,deploy build/$PROJNAME-fixed.wasm build/$PROJNAME-pruned.wasm
