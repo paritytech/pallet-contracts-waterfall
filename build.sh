a22751076aeb61a5332562fa94339c852cc921b2
Stefanie Doll <stefie.doll@googlemail.com>
2020-06-26 11:51:08 +0200

    use submodules instead of clone in CI

--- build.sh
+++ build.sh
@@ -8,9 +8,6 @@ set -evu
 provide-parity-tools
 provide-wabt
 
-git submodule update --init
-git submodule update --remote --merge
-
 echo "____Building Solang Examples____"
 ## Solang installation depends on docker locally and is pre-installed in the CI
 if which docker || [ -n "$CI_JOB_ID" ]; then
@@ -25,13 +22,6 @@ else
     echo "Please install and run Docker if you want to compile the Solang contracts and succesfully run their tests."; 
 fi
 
-echo "____Building ink! Examples____"
-
-cd lib/ink/examples/flipper
-cargo +nightly contract build
-cargo +nightly contract generate-metadata
-cd -
-
 echo "____Building raw Rust Examples____"
 cd contracts/rust/raw-incrementer
 ./build.sh
@@ -41,8 +31,17 @@ cd contracts/rust/restore-contract
 ./build.sh
 cd -
 
-echo "____Building AssemblyScript examples____"
+echo "____Initialize and update git submodules to use latest ink! and as-substrate master____"
+git submodule update --init --recursive
 
+echo "____Building ink! Examples____"
+cd lib/ink/examples/flipper
+cargo +nightly contract build
+cargo +nightly contract generate-metadata
+cd -
+
+
+echo "____Building AssemblyScript examples____"
 cd lib/as-substrate
 yarn clean
 yarn
